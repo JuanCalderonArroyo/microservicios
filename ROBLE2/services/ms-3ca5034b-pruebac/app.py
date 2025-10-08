930e1811591db27d14aae755e5f1b5248b6bd6ee
JuanCalderonArroyo <145511275+JuanCalderonArroyo@users.noreply.github.com>
2025-10-08 14:31:39 -0500

    Delete ROBLE2 directory

--- ROBLE2/services/ms-3ca5034b-pruebac/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, jsonify
-app = Flask(__name__)
-@app.route("/chao", methods=["GET"])
-def bye():
-	return jsonify({"mensaje": "Hasta el inicio..."})
-if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=8000)
